4b58448ae0c3d66869983baf9904da81549aca6e
Leandro Vieira <leandrown@outlook.com>
2020-03-23 15:33:31 -0300

    Dia 1 - Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({
+        evento: 'Semana Omnistack 11',
+        aluno: 'Leandro Vieira'
+    });
+});
+
+app.listen(3333);
