a299c8610e1674cbf7d4f0f9dbea31cc20e2b4b7
Leandro Vieira <leandrown@outlook.com>
2020-03-24 11:25:22 -0300

    Node & Express, Rotas e recursos, e métodos HTTP

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+/**
+ * Métodos HTTP:
+ *
+ * GET: Buscar uma inforação no back-end
+ * POST: Criar  uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+*/
+
+// Rota / Recurso
 app.get('/', (request, response) => {
     return response.json({
         evento: 'Semana Omnistack 11',
