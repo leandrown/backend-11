7f37c0e046e188ff22b21d1adf3de433e901267d
Leandro Vieira <leandrown@outlook.com>
2020-03-24 17:17:27 -0300

    Listar casos específicos de uma ONG

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -30,8 +30,8 @@ module.exports = {
          .select('ong_id')
          .first();
 
-      if(incident.ong_id != ong_id) {
-         return response.status(401).json({ error: 'Operation not permitted'});
+      if (incident.ong_id != ong_id) {
+         return response.status(401).json({ error: 'Operation not permitted.' });
       }
 
       await connection('incidents').where('id', id).delete();
