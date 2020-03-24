47e121e69a76df28a4351b1058238b9b82d921b8
Leandro Vieira <leandrown@outlook.com>
2020-03-24 14:14:44 -0300

    Migrations criadas

--- src/routes.js
+++ src/routes.js
@@ -1,34 +1,6 @@
 const express = require('express');
 const routes = express.Router();
 
-/**
- * Métodos HTTP:
- *
- * GET: Buscar ou listar uma inforação no back-end
- * POST: Criar  uma informação no back-end
- * PUT: Alterar uma informação no back-end
- * DELETE: Deletar uma informação no back-end
-*/
-
-/**
- * Tipos de parâmetros:
- *
- * Query Params: Parâmetros nomeados enviados na rota após "?" (filtros, paginação)
- * Route Params: Parâmetros utilizados para identificar recursos
- * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
-*/
-
-/**
- * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
- * NoSQL: MongoDB, Firebase, CouchDB, etc.
- */
-
-/**
- * Driver: SELECT * FROM users
- * Query Builder: table('users').select('*').where() <-- usaremos essa abordagem com Knex.js
- */
-
-// Rota / Recurso
 routes.post('/users', (request, response) => {
    const body = request.body;
 
