9c2615f215f68541a4798009e710e416e297323e
Leandro Vieira <leandrown@outlook.com>
2020-03-25 22:17:32 -0300

    CORS error detectado

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const routes = require('./routes');
 
 const app = express();
 
+// app.use((request, response) => {
+//    response.header('Access-Control-Allow-Origin', '*');
+//    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+// });
 app.use(cors()); // TODO React com CORS: corrigir bug
 app.use(express.json());
 app.use(routes);
