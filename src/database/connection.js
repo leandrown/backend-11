35b2b3fa97accd528bc8ea2e50309a001fb3ea27
Leandro Vieira <leandrown@outlook.com>
2020-03-27 14:42:45 -0300

    Configurado banco de testes

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const configKnex = require('../../knexfile');
 
-const connection = knex(configKnex.development);
+const config = process.env.NODE_ENV === 'test' ? configKnex.test : configKnex.development;
+
+const connection = knex(config);
 
 module.exports = connection;
